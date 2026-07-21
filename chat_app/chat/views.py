5c4c6f92686c43abf98ef5244462eacebe01ea77
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2026-07-21 20:45:38 +0530

    Refactor checkview function and add leave_room functionality for room management

--- chat_app/chat/views.py
+++ chat_app/chat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Room,Message
+import json 
 from django.http import HttpResponse,JsonResponse
 # Create your views here.
 def index(request):  
@@ -9,16 +10,16 @@ def room(request,pk):
     username=request.GET.get('username')
     room_detail=Room.objects.get(name=pk)
     return render(request,'room.html',{'username':username,'room_details':room_detail,'room':pk})
-def checkview(request):  
-    room=request.POST['room_name']
-    username=request.POST['username']
-    
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
     if Room.objects.filter(name=room).exists():
-        return redirect('/'+room+'/?username='+username)
+        room_obj = Room.objects.get(name=room)
+        room_obj.users += 1
+        room_obj.save()
     else:
-        new_room=Room.objects.create(name=room)
-        new_room.save()
-        return redirect('/'+room+'/?username='+username)
+        room_obj = Room.objects.create(name=room, users=1)
+    return redirect('/' + room + '/?username=' + username)
 def send(request):
     message=request.POST['message']
     username=request.POST['username']
@@ -31,3 +32,18 @@ def getmessages(request,pk):
     room_details=Room.objects.get(name=pk)
     message=Message.objects.filter(room=room_details.id)
     return JsonResponse({"messages":list(message.values())})
+def leave_room(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        room = Room.objects.get(name=data["room"])
+
+        room.users -= 1
+
+        if room.users <= 0:
+            Message.objects.filter(room=str(room.id)).delete()
+            room.delete()
+        else:
+            room.save()
+
+        return JsonResponse({"status": "success"})
