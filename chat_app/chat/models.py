b76cdcb14a80227f24d580a35cd82f36bc04e488
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2026-07-21 20:45:20 +0530

    Format code and add 'users' field to Room model

--- chat_app/chat/models.py
+++ chat_app/chat/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from datetime import datetime
-# Create your models here.
+
 class Room(models.Model):
-    name=models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    users = models.PositiveIntegerField(default=0)   # New Field
 
 class Message(models.Model):
-    value=models.CharField(max_length=10000000)
-    date=models.DateTimeField(default=datetime.now,blank=True)
-    user=models.CharField(max_length=1000000)
-    room=models.CharField(max_length=1000000)
+    value = models.CharField(max_length=10000000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=1000000)
+    room = models.CharField(max_length=1000000)
